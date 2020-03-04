a9b33102f48182f2ccca847a6e41b81970da1ad6
DiegoPerez1441 <diegooperez14@gmail.com>
2020-03-03 23:31:58 -0600

    Fixed UI Bugs

--- front-end/src/components/Overview.js
+++ front-end/src/components/Overview.js
@@ -3,7 +3,20 @@ import React from 'react';
 class Overview extends React.Component {
   render () {
     return(
-     <h1>Overview</h1>
+      <div className="Overview">
+
+        <div className="selected">
+          <div className="menu-icon active"><i class="fas fa-bars"></i></div>
+          <h2>All Notes</h2>
+        </div>
+
+        
+        <div className="note_file"></div>
+        <div className="note_file"></div>
+        <div className="note_file"></div>
+        <div className="note_file"></div>
+
+      </div>
     )
   }
 }
