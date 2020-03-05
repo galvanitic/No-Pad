7e48d7d77ec7446ac7e703d28d0f2fe1f3835843
DiegoPerez1441 <diegooperez14@gmail.com>
2020-03-04 22:51:00 -0600

    Added Log In Page and Fields

--- front-end/src/components/LogIn.js
+++ front-end/src/components/LogIn.js
@@ -5,7 +5,24 @@ class LogIn extends React.Component {
   render () {
     return(
       <div id="login-container">
-        <div id="login-form"></div>
+        <div id="login-form">
+          <h2>Log In</h2>
+          <h4>Sign into your account to continue</h4>
+
+          <div className="form-section" id='email-container'>
+            <h5>Email Address</h5>
+            <div className="input-field" id='email-field'></div>
+          </div>
+
+          <div className="form-section" id='password-container'>
+            <h5>Password</h5>
+            <div className="input-field" id='password-field'></div>
+          </div>
+
+          <div id="signin"><h3>Sign In</h3></div>
+
+          <h5 id="createAccount-text">Not Registered? <span>Create Account</span></h5>
+        </div>
       </div>
     )
   }
