b37fb24d5601619c3b22c04ab32e929358a4a037
DiegoPerez1441 <diegooperez14@gmail.com>
2020-03-04 22:11:23 -0600

    Added FontAwesome dependancies and modified code to make it work

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Router from './Router'
 import { BrowserRouter } from "react-router-dom";
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCaretRight, faFileAlt, faStar, faFolder, faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCaretRight, faFileAlt, faStar, faFolder, faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog);
+
 function App() {
   return (
     <BrowserRouter>
--- front-end/src/components/SideNav.js
+++ front-end/src/components/SideNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import portrait from '../img/portrait.jpg'
 class SideNav extends React.Component {
   render () {
@@ -23,26 +24,26 @@ class SideNav extends React.Component {
             <li><span className="fa-li"><i className="far fa-square"></i></span>Shared with Me</li>
           </ul> --> */}
           <ul className="QuickAccess_list fa-ul">
-            <li><span className="fa-li"><i className=""></i></span><i className="fas fa-file-alt"></i>All Notes</li>
-            <li><span className="fa-li"><i className="fas fa-caret-right expand-icon"></i></span><i className="fas fa-star"></i>Important</li>
-            <li><span className="fa-li"><i className="fas fa-caret-right expand-icon"></i></span><i className="fas fa-folder"></i>Folders</li>
-            <li><span className="fa-li"><i className="fas fa-caret-right expand-icon"></i></span><i className="fas fa-user-friends"></i>Shared with Me</li>
+            <li><span className="fa-li"><i className=""></i></span><FontAwesomeIcon icon={['fas', 'file-alt']} />All Notes</li>
+            <li><span className="fa-li"><FontAwesomeIcon icon={['fas', 'caret-right']} /></span><FontAwesomeIcon icon={['fas', 'star']} />Important</li>
+            <li><span className="fa-li"><FontAwesomeIcon icon={['fas', 'caret-right']} /></span><FontAwesomeIcon icon={['fas', 'folder']} />Folders</li>
+            <li><span className="fa-li"><FontAwesomeIcon icon={['fas', 'caret-right']} /></span><FontAwesomeIcon icon={['fas', 'user-friends']} />Shared with Me</li>
           </ul>
         </div>
 
         <div className="Workspaces">
           <h2>Workspaces</h2>
           <ul className="Workspaces_list fa-ul">
-            <li><span className="fa-li"><i className=""></i></span><i className="fas fa-briefcase"></i>Work</li>
-            <li><span className="fa-li"><i className=""></i></span><i className="fas fa-chalkboard-teacher"></i>School</li>
-            <li><span className="fa-li"><i className=""></i></span><i className="fas fa-user"></i>Personal</li>
+            <li><span className="fa-li"><i className=""></i></span><FontAwesomeIcon icon={['fas', 'briefcase']} />Work</li>
+            <li><span className="fa-li"><i className=""></i></span><FontAwesomeIcon icon={['fas', 'chalkboard-teacher']} />School</li>
+            <li><span className="fa-li"><i className=""></i></span><FontAwesomeIcon icon={['fas', 'user']} />Personal</li>
           </ul>
         </div>
 
         <div className="Misc">
           <ul className="Misc_list fa-ul">
-            <li><i className="fas fa-trash"></i>Trash</li>
-            <li><i className="fas fa-cog"></i>Settings</li>
+            <li><FontAwesomeIcon icon={['fas', 'trash']} />Trash</li>
+            <li><FontAwesomeIcon icon={['fas', 'cog']} />Settings</li>
           </ul>
         </div>
      </aside>
