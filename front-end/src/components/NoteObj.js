6c0b41ea6fa7193c63c9085b2d9f34e4b453f5fe
DiegoPerez1441 <diegooperez14@gmail.com>
2020-03-16 16:33:31 -0500

    Added overview-cards as buttons

--- front-end/src/components/NoteObj.js
+++ front-end/src/components/NoteObj.js
@@ -5,9 +5,16 @@ const NoteObj = ({ title, content, lastModified }) => (
 
   // Need to change div to a button to be 'selectable' by the user
   <div className="NoteObj">
-    <h2>{title}</h2>
+    {/* <h2>{title}</h2>
     <p>{content}</p>
-    <h6>{lastModified}</h6>
+    <h6>{lastModified}</h6> */}
+
+    <button className="NoteObj_Button">
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <h6>{lastModified}</h6>
+    </button>
+
   </div>
 )
 
