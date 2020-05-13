4687ddcc56c19aca9bb08f9217b6daf056fa9ea6
DiegoPerez1441 <diegooperez14@gmail.com>
2020-05-13 16:03:15 -0500

    Added New Note button

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,9 +3,12 @@ import Router from './Router'
 import { BrowserRouter } from "react-router-dom";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCaretRight, faFileAlt, faStar, faFolder, faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog } from '@fortawesome/free-solid-svg-icons';
+import { 
+  faPlusSquare, faCaretRight, faFileAlt, faStar, faFolder, 
+  faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog 
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCaretRight, faFileAlt, faStar, faFolder, faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog);
+library.add(faPlusSquare, faCaretRight, faFileAlt, faStar, faFolder, faUserFriends, faBriefcase, faChalkboardTeacher, faUser, faTrash, faCog);
 
 function App() {
   return (
