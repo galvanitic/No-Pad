79c2a1a7696c54b4c3ca6f2ace379b4bd3438082
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-14 14:57:09 -0500

    Added user selection of note in Overview

--- front-end/src/components/Note.js
+++ front-end/src/components/Note.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
-const Note = ( {user, noteId} ) => {
+const Note = ( {user, note} ) => {
 
-  const [note, setNote] = useState(null)
+  // const [note, setNote] = useState(null)
 
   
   return(
     <div className="Note">
-      {/* <p>{noteId}</p> */}
+      <h1>{(note !== null) ? note.title : ""}</h1>
+      <p>{(note !== null) ? note.content : ""}</p>
     </div>
   )
   
