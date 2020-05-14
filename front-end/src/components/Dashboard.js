62b9f7beab8af81c17510325a17e23818b63cf6c
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-14 14:16:44 -0500

    Cleaned up code

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import SideNav from './SideNav';
 import Note from './Note';
 import Overview from './Overview';
