650cfd7541fb70a6688cafba846289cd48db3d45
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-14 17:13:38 -0500

    Updating Note Data **Bug In this Commit**

--- front-end/src/components/Note.js
+++ front-end/src/components/Note.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState, useCallback } from 'react';
 
+import { 
+  updateNoteForUser
+} from '../utils/localStorageUtils'
+
 const Note = ( {user, note} ) => {
+
+  const [noteContent, setNoteContent] = useState((note !== null) ? note.content : null)
+
+  const handleNoteContentChange = (e) => {
+    const newNoteContent = e.target.value
+    setNoteContent(newNoteContent)
+    note.content = noteContent
+
+    updateNoteForUser(note)
+  }
 
   const updateNote = useCallback((e) => {
     e.preventDefault();
@@ -10,8 +24,7 @@ const Note = ( {user, note} ) => {
   return(
     <div className="Note">
       <h2>{(note !== null) ? note.title : ""}</h2>
-      {/* <textarea>{(note !== null) ? note.content : ""}</textarea> */}
-      {(note !== null) ? <textarea>{note.content}</textarea> : <></>}
+      {(note !== null) ? <textarea value={noteContent} onChange={handleNoteContentChange}></textarea> : <></>}
 
       {(note !== null) ? <button type="submit" onClick={updateNote} className="button_form">Submit</button> : <></>}
     </div>
