4d04c49ffe480f1b07daa833a0f357335df04c27
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-16 14:30:35 -0500

    Added Beta Build for No-Pad

---
+++ front-end/build/precache-manifest.e76a2686840ad71e7fc314aada73ce9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f4e77a79ac08718077242346efb691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e04453686bffccde0f6",
+    "url": "/static/css/main.0ba10cf1.chunk.css"
+  },
+  {
+    "revision": "124dbdd90ff043ab558d",
+    "url": "/static/js/2.906e6b32.chunk.js"
+  },
+  {
+    "revision": "32f87eb505bf8f72368a9953caeea869",
+    "url": "/static/js/2.906e6b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e04453686bffccde0f6",
+    "url": "/static/js/main.3f814435.chunk.js"
+  },
+  {
+    "revision": "673d8ffee3e8ed730e63",
+    "url": "/static/js/runtime-main.a0a66673.js"
+  },
+  {
+    "revision": "c2b130ef6058a831d781bce185f85647",
+    "url": "/static/media/LogInScreen-background.c2b130ef.jpg"
+  },
+  {
+    "revision": "0d5a23fc9f60ec4f1e084529a61ab2bd",
+    "url": "/static/media/defaultProfilePicture.0d5a23fc.png"
+  }
+]);
