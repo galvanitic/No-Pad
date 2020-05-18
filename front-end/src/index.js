9a98ad5f778dbdf99f42453c390b5276bec06b18
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-05-18 13:54:06 -0500

    Installed react-quill and firebaseConfig info

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,6 +5,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp69kbtuqhHU1RFFPjudKzCOzAWSPkxeU",
+  authDomain: "no-pad.firebaseapp.com",
+  databaseURL: "https://no-pad.firebaseio.com",
+  projectId: "no-pad",
+  storageBucket: "no-pad.appspot.com",
+  messagingSenderId: "145117556594",
+  appId: "1:145117556594:web:0e8648c111cdecbd6db7eb",
+  measurementId: "G-14JQ9F0NJG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
